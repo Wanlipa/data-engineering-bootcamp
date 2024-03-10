a1cda2573beb65230c03d12320aefdf6ee43e05c
Wanlipa <nuam18@gmail.com>
2024-03-10 08:50:47 +0000

    airflow template

--- 04-data-pipelines-with-apache-airflow/dags/play_with_airflow_connections_and_hooks.py
+++ 04-data-pipelines-with-apache-airflow/dags/play_with_airflow_connections_and_hooks.py
@@ -3,6 +3,8 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils import timezone
 
+import great_expectations as ge
+
 
 def _get_data():
     pg_hook = PostgresHook(
@@ -29,6 +31,17 @@ def _dump_data(table: str):
     pg_hook.bulk_dump(table, f"/opt/airflow/dags/{table}_export")
     
 
+def _validate_data():
+    columns = ["product_id", "name", "price", "inventory"]
+    my_df = ge.read_csv("/opt/airflow/dags/products_export", names=columns, sep="\t")
+    results = my_df.expect_column_values_to_be_between(
+        column="price",
+        min_value=0,
+        max_value=95,
+    )
+    assert results["success"] is True
+
+
 with DAG(
     dag_id="play_with_airflow_connections_and_hooks",
     schedule="@daily",
@@ -47,3 +60,10 @@ with DAG(
         python_callable=_dump_data,
         op_kwargs={"table": "products"},
     )
+
+    validate_data = PythonOperator(
+        task_id="validate_data",
+        python_callable=_validate_data,
+    )
+
+    dump_product_data >> validate_data
